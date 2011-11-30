25117f9e95831628372c3f5cb2f86c2b700dd2d8
Allen Byrne <byrn@hdfgroup.org>
2011-11-30 09:14:27 -0500

    [svn-r21780] Remove private scripts

--- cmakePsvnconfigbuildtest.sh
+++
@@ -1,4 +0,0 @@
-cd pbuild
-cmake -C ../config/cmake/cacheinit.cmake -G "Unix Makefiles" -DSYSTEM_NAME_EXT=Fedora_13_x64_PAR -DHDF5_BUILD_CPP_LIB:BOOL=OFF -DHDF5_ENABLE_PARALLEL:BOOL=ON -DHDF5_ALLOW_EXTERNAL_SUPPORT:STRING=SVN ..
-ctest -D Experimental -A ../config/cmake/cacheinit.cmake
-cd ..
--- cmakesvnconfigbuild.sh
+++
@@ -1,4 +0,0 @@
-cd build
-cmake -C ../config/cmake/cacheinit.cmake -G "Unix Makefiles" -DSYSTEM_NAME_EXT=Fedora_13_x64 -DHDF5_ALLOW_EXTERNAL_SUPPORT:STRING=SVN ..
-cmake --build .
-cd ..
