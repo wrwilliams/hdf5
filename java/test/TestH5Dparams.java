e2068d703a7a327ffab13cd24b64977d56fc5205
Allen Byrne <byrn@hdfgroup.org>
2015-05-28 09:11:46 -0500

    [svn-r27117] These are the java files needed for trunk. CMake files are correct (unable to compile C Library with CMake) The makefile.am files are templates with correct source files and need more work. The java folder should replace jni folder.

---
+++ java/test/TestH5Dparams.java
@@ -0,0 +1,131 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+import hdf.hdf5lib.H5;
+import hdf.hdf5lib.exceptions.HDF5LibraryException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+public class TestH5Dparams {
+    @Rule public TestName testname = new TestName();
+
+    @Before
+    public void checkOpenIDs() {
+        assertTrue("H5 open ids is 0",H5.getOpenIDCount()==0);
+        System.out.print(testname.getMethodName());
+    }
+    @After
+    public void nextTestName() {
+        System.out.println();
+    }
+
+    @Test//(expected = HDF5LibraryException.class)
+    public void testH5Dclose_invalid() throws Throwable {
+    	int did = H5.H5Dclose(-1);
+        assertTrue(did == 0);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testH5Dcreate_null() throws Throwable {
+        H5.H5Dcreate(-1, null, 0, 0, 0, 0, 0);
+    }
+
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Dcreate_invalid() throws Throwable {
+        H5.H5Dcreate(-1, "Bogus", -1, -1, -1, -1, -1);
+    }
+
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Dcreate_anon_invalid() throws Throwable {
+        H5.H5Dcreate_anon(-1, -1, -1, -1, -1);
+    }
+
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Dget_access_plist_invalid() throws Throwable {
+        H5.H5Dget_access_plist(-1);
+    }
+
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Dget_create_plist_invalid() throws Throwable {
+        H5.H5Dget_create_plist(-1);
+    }
+
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Dget_offset_invalid() throws Throwable {
+        H5.H5Dget_offset(-1);
+    }
+
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Dget_space_invalid() throws Throwable {
+        H5.H5Dget_space(-1);
+    }
+
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Dget_type_invalid() throws Throwable {
+        H5.H5Dget_type(-1);
+    }
+
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Dget_space_status_invalid() throws Throwable {
+        int[] status = new int[2];
+        H5.H5Dget_space_status(-1, status);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testH5Dget_space_status_null() throws Throwable {
+        H5.H5Dget_space_status(-1, null);
+    }
+
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Dset_extent_status_invalid() throws Throwable {
+        long[] size = new long[2];
+        H5.H5Dset_extent(-1, size);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testH5Dset_extent_status_null() throws Throwable {
+        H5.H5Dset_extent(-1, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testH5Dopen_null() throws Throwable {
+        H5.H5Dopen(-1, null, 0);
+    }
+
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Dopen_invalid() throws Throwable {
+        H5.H5Dopen(-1, "Bogus", 0);
+    }
+
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Dvlen_get_buf_size_invalid() throws Throwable {
+        int[] size = new int[2];
+        H5.H5Dvlen_get_buf_size(-1, -1, -1, size);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testH5Dvlen_get_buf_size_null() throws Throwable {
+        H5.H5Dvlen_get_buf_size(-1, -1, -1, null);
+    }
+
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Dvlen_reclaim_invalid() throws Throwable {
+        byte[] buf = new byte[2];
+        H5.H5Dvlen_reclaim(-1, -1, -1, buf);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testH5Dvlen_reclaim_null() throws Throwable {
+        H5.H5Dvlen_reclaim(-1, -1, -1, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testH5Dget_storage_size_invalid() throws Throwable {
+        H5.H5Dget_storage_size(-1);
+    }
+
+}
---
+++ java/test/TestH5Z.java
@@ -0,0 +1,85 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import hdf.hdf5lib.H5;
+import hdf.hdf5lib.HDF5Constants;
+import hdf.hdf5lib.exceptions.HDF5LibraryException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+public class TestH5Z {
+    @Rule public TestName testname = new TestName();
+
+    @Before
+    public void checkOpenIDs() {
+        assertTrue("H5 open ids is 0",H5.getOpenIDCount()==0);
+        System.out.print(testname.getMethodName());
+    }
+    @After
+    public void nextTestName() {
+        System.out.println();
+    }
+    
+    @Test
+    public void testH5Zfilter_avail() {
+        try {
+            int filter_found = H5.H5Zfilter_avail(HDF5Constants.H5Z_FILTER_DEFLATE);
+            assertTrue("H5.H5Zfilter_avail", filter_found > 0);
+            filter_found = H5.H5Zfilter_avail(HDF5Constants.H5Z_FILTER_FLETCHER32);
+            assertTrue("H5.H5Zfilter_avail", filter_found > 0);
+            filter_found = H5.H5Zfilter_avail(HDF5Constants.H5Z_FILTER_NBIT);
+            assertTrue("H5.H5Zfilter_avail", filter_found > 0);
+            filter_found = H5.H5Zfilter_avail(HDF5Constants.H5Z_FILTER_SCALEOFFSET);
+            assertTrue("H5.H5Zfilter_avail", filter_found > 0);
+            filter_found = H5.H5Zfilter_avail(HDF5Constants.H5Z_FILTER_SHUFFLE);
+            assertTrue("H5.H5Zfilter_avail", filter_found > 0);
+            filter_found = H5.H5Zfilter_avail(HDF5Constants.H5Z_FILTER_SZIP);
+            assertTrue("H5.H5Zfilter_avail", filter_found > 0);
+        }
+        catch (Throwable err) {
+            err.printStackTrace();
+            fail("H5.H5Zfilter_avail " + err);
+        }
+    }
+    
+    @Test
+    public void testH5Zget_filter_info() {
+        try {
+            int filter_flag = H5.H5Zget_filter_info(HDF5Constants.H5Z_FILTER_DEFLATE);
+            assertTrue("H5.H5Zget_filter_info", (filter_flag & HDF5Constants.H5Z_FILTER_CONFIG_DECODE_ENABLED) > 0);
+            assertTrue("H5.H5Zget_filter_info", (filter_flag & HDF5Constants.H5Z_FILTER_CONFIG_ENCODE_ENABLED) > 0);
+            filter_flag = H5.H5Zget_filter_info(HDF5Constants.H5Z_FILTER_FLETCHER32);
+            assertTrue("H5.H5Zget_filter_info", (filter_flag & HDF5Constants.H5Z_FILTER_CONFIG_DECODE_ENABLED) > 0);
+            assertTrue("H5.H5Zget_filter_info", (filter_flag & HDF5Constants.H5Z_FILTER_CONFIG_ENCODE_ENABLED) > 0);
+            filter_flag = H5.H5Zget_filter_info(HDF5Constants.H5Z_FILTER_NBIT);
+            assertTrue("H5.H5Zget_filter_info", (filter_flag & HDF5Constants.H5Z_FILTER_CONFIG_DECODE_ENABLED) > 0);
+            assertTrue("H5.H5Zget_filter_info", (filter_flag & HDF5Constants.H5Z_FILTER_CONFIG_ENCODE_ENABLED) > 0);
+            filter_flag = H5.H5Zget_filter_info(HDF5Constants.H5Z_FILTER_SCALEOFFSET);
+            assertTrue("H5.H5Zget_filter_info", (filter_flag & HDF5Constants.H5Z_FILTER_CONFIG_DECODE_ENABLED) > 0);
+            assertTrue("H5.H5Zget_filter_info", (filter_flag & HDF5Constants.H5Z_FILTER_CONFIG_ENCODE_ENABLED) > 0);
+            filter_flag = H5.H5Zget_filter_info(HDF5Constants.H5Z_FILTER_SHUFFLE);
+            assertTrue("H5.H5Zget_filter_info", (filter_flag & HDF5Constants.H5Z_FILTER_CONFIG_DECODE_ENABLED) > 0);
+            assertTrue("H5.H5Zget_filter_info", (filter_flag & HDF5Constants.H5Z_FILTER_CONFIG_ENCODE_ENABLED) > 0);
+            filter_flag = H5.H5Zget_filter_info(HDF5Constants.H5Z_FILTER_SZIP);
+            assertTrue("H5.H5Zget_filter_info", (filter_flag & HDF5Constants.H5Z_FILTER_CONFIG_DECODE_ENABLED) > 0);
+            assertTrue("H5.H5Zget_filter_info", (filter_flag & HDF5Constants.H5Z_FILTER_CONFIG_ENCODE_ENABLED) > 0);
+        }
+        catch (Throwable err) {
+            err.printStackTrace();
+            fail("H5.H5Zget_filter_info " + err);
+        }
+    }
+    
+    @Test(expected = HDF5LibraryException.class)
+    public void testH5Zunregister_predefined() throws Throwable {
+        int filter_found = H5.H5Zfilter_avail(HDF5Constants.H5Z_FILTER_SHUFFLE);
+        assertTrue("H5.H5Zfilter_avail", filter_found > 0);
+        
+        H5.H5Zunregister(HDF5Constants.H5Z_FILTER_SHUFFLE);
+    }
+}
