406ed2624c387ec75a88526bfe42d4f800cad564
Allen Byrne <byrn@hdfgroup.org>
2016-02-15 15:01:21 -0500

    [svn-r29121] Add comments (follow changes in exception.* files) HDF5-341: review changes implemented

--- java/src/jni/h5zImp.c
+++ java/src/jni/h5zImp.c
@@ -34,34 +34,32 @@ extern "C" {
  * Method:    H5Zunregister
  * Signature: (I)I
  */
-JNIEXPORT jint JNICALL Java_hdf_hdf5lib_H5_H5Zunregister
-  (JNIEnv *env, jclass clss, jint filter)
+JNIEXPORT jint JNICALL
+Java_hdf_hdf5lib_H5_H5Zunregister(JNIEnv *env, jclass clss, jint filter)
 {
     herr_t retValue = H5Zunregister((H5Z_filter_t)filter);
 
-    if (retValue < 0) {
+    if (retValue < 0)
         h5libraryError(env);
-    }
 
     return (jint)retValue;
-}
+} /* end Java_hdf_hdf5lib_H5_H5Zunregister */
 
 /*
  * Class:     hdf_hdf5lib_H5
  * Method:    H5Zfilter_avail
  * Signature: (I)I
  */
-JNIEXPORT jint JNICALL Java_hdf_hdf5lib_H5_H5Zfilter_1avail
-  (JNIEnv *env, jclass clss, jint filter)
+JNIEXPORT jint JNICALL
+Java_hdf_hdf5lib_H5_H5Zfilter_1avail(JNIEnv *env, jclass clss, jint filter)
 {
     herr_t retValue = H5Zfilter_avail((H5Z_filter_t)filter);
 
-    if (retValue < 0) {
+    if (retValue < 0)
         h5libraryError(env);
-    }
 
     return (jint)retValue;
-}
+} /* end Java_hdf_hdf5lib_H5_H5Zfilter_1avail */
 
 /*
  * Class:     hdf_hdf5lib_H5
@@ -69,17 +67,16 @@ JNIEXPORT jint JNICALL Java_hdf_hdf5lib_H5_H5Zfilter_1avail
  * Signature: (I)I
  */
 
-JNIEXPORT jint JNICALL Java_hdf_hdf5lib_H5_H5Zget_1filter_1info
-  (JNIEnv *env, jclass clss, jint filter)
+JNIEXPORT jint JNICALL
+Java_hdf_hdf5lib_H5_H5Zget_1filter_1info(JNIEnv *env, jclass clss, jint filter)
 {
     unsigned int flags = 0;
 
-    if (H5Zget_filter_info ((H5Z_filter_t) filter, (unsigned *) &flags) < 0) {
+    if (H5Zget_filter_info ((H5Z_filter_t) filter, (unsigned *) &flags) < 0)
         h5libraryError(env);
-    }
 
     return (jint)flags;
-}
+} /* end Java_hdf_hdf5lib_H5_H5Zget_1filter_1info */
 
 
 #ifdef __cplusplus
