6b5bfa406259fc03f03e292b56f9d335744e3ead
Binh-Minh Ribler <bmribler@hdfgroup.org>
2016-10-12 14:21:57 -0500

    Description:     Fixed typo that caused daily test failed when --enable-deprecated-symbols     is used.  Also, removed a commented-out function. Platforms tested:     Linux/32 2.6 (jam)     Linux/64 (platypus) with --enable-deprecated-symbols

--- c++/src/H5Location.cpp
+++ c++/src/H5Location.cpp
@@ -2158,6 +2158,9 @@ H5G_obj_t H5Location::getObjTypeByIdx(hsize_t idx, H5std_string& type_name) cons
     return (obj_type);
 }
 
+#endif // DOXYGEN_SHOULD_SKIP_THIS
+#endif /* H5_NO_DEPRECATED_SYMBOLS */
+
 //--------------------------------------------------------------------------
 // Function:    H5Location::throwException
 ///\brief       Invokes subclass' throwException
@@ -2170,9 +2173,6 @@ void H5Location::throwException(const H5std_string& func_name, const H5std_strin
 {
    throwException(func_name, msg);
 }
-
-#endif // DOXYGEN_SHOULD_SKIP_THIS
-#endif /* H5_NO_DEPRECATED_SYMBOLS */
 
 //--------------------------------------------------------------------------
 // Function:    f_DataType_setId - friend
