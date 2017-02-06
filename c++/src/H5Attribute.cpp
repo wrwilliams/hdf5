ee948a8a850af26b1ef7ed286e27a623c2db1bce
Quincey Koziol <koziol@hdfgroup.org>
2017-02-06 14:15:17 -0800

    Merge in recent C++ changes from the trunk.

--- c++/src/H5Attribute.cpp
+++ c++/src/H5Attribute.cpp
@@ -20,6 +20,7 @@
 #endif
 #include <string>
 
+#include "H5private.h"		// for HDfree
 #include "H5Include.h"
 #include "H5Exception.h"
 #include "H5IdComponent.h"
@@ -28,13 +29,13 @@
 #include "H5FcreatProp.h"
 #include "H5OcreatProp.h"
 #include "H5DcreatProp.h"
+#include "H5LaccProp.h"
 #include "H5Location.h"
 #include "H5Object.h"
 #include "H5AbstractDs.h"
 #include "H5DataType.h"
 #include "H5DataSpace.h"
 #include "H5Attribute.h"
-#include "H5private.h"		// for HDfree
 
 namespace H5 {
 using std::cerr;
--- c++/src/H5CompType.cpp
+++ c++/src/H5CompType.cpp
@@ -22,13 +22,13 @@
 #include "H5OcreatProp.h"
 #include "H5DcreatProp.h"
 #include "H5DxferProp.h"
+#include "H5LaccProp.h"
 #include "H5Location.h"
 #include "H5Object.h"
 #include "H5Alltypes.h"
 #include "H5AbstractDs.h"
 #include "H5DataSpace.h"
 #include "H5DataSet.h"
-#include "H5private.h"
 
 namespace H5 {
 
