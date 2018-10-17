fb681c31deb0a73d6a8d70c02ac24e66d66f16d6
Jordan Henderson <jhenderson@hdfgroup.org>
2018-10-17 16:11:47 -0500

    Temporary functions added for DAOS VOL support

--- src/H5Spublic.h
+++ src/H5Spublic.h
@@ -149,6 +149,13 @@ H5_DLL herr_t H5Sget_select_bounds(hid_t spaceid, hsize_t start[],
     hsize_t end[]);
 H5_DLL H5S_sel_type H5Sget_select_type(hid_t spaceid);
 
+/* The following have been temporarily added to support the DAOS VOL plugin */
+H5_DLL herr_t H5Sselect_iter_init(hid_t space_id, size_t elmt_size, void **sel_iter);
+H5_DLL herr_t H5Sselect_iter_release(void **sel_iter);
+H5_DLL herr_t H5Sselect_get_seq_list(hid_t space_id, unsigned flags, void **sel_iter,
+                                     size_t maxseq, size_t maxbytes, size_t *nseq,
+                                     size_t *nbytes, hsize_t *off, size_t *len);
+
 #ifdef __cplusplus
 }
 #endif
